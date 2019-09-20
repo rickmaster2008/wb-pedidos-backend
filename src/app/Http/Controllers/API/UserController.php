925ac21a5fca6ed83bfd8e81fc2a479bfc75fd8b
Ricardo Cotillo <ricardo@pop-os.localdomain>
2019-09-20 10:39:42 -0500

    fixed usercontroller

--- src/app/Http/Controllers/API/UserController.php
+++ src/app/Http/Controllers/API/UserController.php
@@ -1,72 +1,75 @@
 <?php
+
 namespace App\Http\Controllers\API;
-use Illuminate\Http\Request; 
-use App\Http\Controllers\Controller; 
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\User;
 use App\Models\Ccmven;
-use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Auth;
 use Validator;
 
-class UserController extends Controller 
+class UserController extends Controller
 {
-    public $successStatus = 200;
-/** 
-     * login api 
-     * 
-     * @return \Illuminate\Http\Response 
-     */ 
-    public function login(){ 
-        $user = User::where('email', request('email'))->where('password', request('password'))->first();
-        if ($user) {
-            $ven = Ccmven::where('user_id', $user['id'])->first();
-            $success;
-            if ($ven) {
-                $success = [
-                    'access_token' => $user->createToken('MyApp')-> accessToken,
-                    'user' => $ven,
-                ];
-            } else {
-                $success = [
-                    'access_token' => $user->createToken('MyApp')-> accessToken,
-                    'user' => Ccmven::all()->first(),
-                ];
-            }
-            return response()->json($success, $this-> successStatus); 
-        } else {
-            return response()->json(['error'=>'Unauthorised'], 401); 
-        }
+  public $successStatus = 200;
+  /** 
+   * login api 
+   * 
+   * @return \Illuminate\Http\Response 
+   */
+  public function login()
+  {
+    $user = User::where('email', request('email'))->where('password', request('password'))->first();
+    if ($user) {
+      $ven = Ccmven::where('MNOMBRE', $user['name'])->first();
+      $success = [];
+      if ($ven) {
+        $success = [
+          'access_token' => $user->createToken('MyApp')->accessToken,
+          'user' => $ven,
+        ];
+      } else {
+        $success = [
+          'access_token' => $user->createToken('MyApp')->accessToken,
+          'user' => Ccmven::all()->first(),
+        ];
+      }
+      return response()->json($success, $this->successStatus);
+    } else {
+      return response()->json(['error' => 'Unauthorised'], 401);
     }
-/** 
-     * Register api 
-     * 
-     * @return \Illuminate\Http\Response 
-     */ 
-    public function register(Request $request) 
-    { 
-        $validator = Validator::make($request->all(), [ 
-            'name' => 'required', 
-            'email' => 'required|email', 
-            'password' => 'required', 
-            'c_password' => 'required|same:password', 
-        ]);
-        if ($validator->fails()) { 
-            return response()->json(['error'=>$validator->errors()], 401);            
-        }
-        $input = $request->all(); 
-        $input['password'] = bcrypt($input['password']); 
-        $user = User::create($input); 
-        $success['token'] =  $user->createToken('MyApp')-> accessToken; 
-        $success['name'] =  $user->name;
-        return response()->json(['success'=>$success], $this-> successStatus); 
+  }
+  /** 
+   * Register api 
+   * 
+   * @return \Illuminate\Http\Response 
+   */
+  public function register(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'name' => 'required',
+      'email' => 'required|email',
+      'password' => 'required',
+      'c_password' => 'required|same:password',
+    ]);
+    if ($validator->fails()) {
+      return response()->json(['error' => $validator->errors()], 401);
     }
-/** 
-     * details api 
-     * 
-     * @return \Illuminate\Http\Response 
-     */ 
-    public function details() 
-    { 
-        $user = Auth::user(); 
-        return response()->json(['success' => $user], $this-> successStatus); 
-    } 
+    $input = $request->all();
+    $input['password'] = bcrypt($input['password']);
+    $user = User::create($input);
+    $success['token'] =  $user->createToken('MyApp')->accessToken;
+    $success['name'] =  $user->name;
+    return response()->json(['success' => $success], $this->successStatus);
+  }
+  /** 
+   * details api 
+   * 
+   * @return \Illuminate\Http\Response 
+   */
+  public function details()
+  {
+    $user = Auth::user();
+    return response()->json(['success' => $user], $this->successStatus);
+  }
 }
